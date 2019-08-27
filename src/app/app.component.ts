805a44d9fe2aa4afb7a37c7c2b669684333d482a
YUSUF HERI <yusufheri64@gmail.com>
2019-08-27 23:09:11 +0200

    Feat: creation de l'entete du page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MaSuperAgence';
+
+  constructor() {
+    const firebaseConfig = {
+      apiKey: 'AIzaSyBDm7nNUgbol4if8EZGSgs7eZcBRbXs0Ys',
+      authDomain: 'masuperagence-8d8da.firebaseapp.com',
+      databaseURL: 'https://masuperagence-8d8da.firebaseio.com',
+      projectId: 'masuperagence-8d8da',
+      storageBucket: '',
+      messagingSenderId: '786507518394',
+      appId: '1:786507518394:web:3469b63598f62b3e'
+    };
+
+    firebase.initializeApp(firebaseConfig);
+  }
 }
