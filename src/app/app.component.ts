ad9e695da74f7cae0ea9d2f49b29c0e909c4948f
YUSUF HERI <32743573+yusufheri@users.noreply.github.com>
2019-08-30 21:40:39 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,12 +10,12 @@ export class AppComponent {
 
   constructor() {
     const firebaseConfig = {
-      apiKey: 'AIzaSyBlv3cR6BthyybkVFQ0gjsX5Tuo4EbEPcs',
+      apiKey: 'key',
       authDomain: 'superagence-dbbed.firebaseapp.com',
       databaseURL: 'https://superagence-dbbed.firebaseio.com',
       projectId: 'superagence-dbbed',
       storageBucket: '',
-      messagingSenderId: '425656949671',
+      messagingSenderId: 'msgId',
       appId: '1:425656949671:web:595481a44b641a89'
     };
     // Initialize Firebase
